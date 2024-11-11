e4a3d18bc12ddafb4db3d48613ffa73aa7444e95
NFJHolmes <NFJHolmes@gmail.com>
2024-11-11 12:55:09 -0500

    init

--- javascript/app/svelte.config.js
+++ javascript/app/svelte.config.js
@@ -1,20 +1,11 @@
 import adapter from '@sveltejs/adapter-netlify';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
-// import { sveltePreprocess } from 'svelte-preprocess';
-// import sequence from 'svelte-sequential-preprocessor';
-
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
     // Consult https://kit.svelte.dev/docs/integrations#preprocessors
     // for more information about preprocessors
     preprocess: vitePreprocess(),
-    // preprocess: sequence([
-    //     vitePreprocess(),
-    //     // sveltePreprocess({
-    //     //     postcss: true,
-    //     // }),
-    // ]),
     kit: {
         // adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
         // If your environment is not supported or you settled on a specific environment, switch out the adapter.
@@ -33,16 +24,12 @@ const config = {
             'script-src': ['self', 'blob:'],
             'connect-src': [
                 'self',
-                // process.env.VITE_BACKEND_BASE_URL,
-                // process.env.VITE_BUNNYCDN_IMAGE_URL,
-                // process.env.VITE_BUNNYCDN_DOCUMENT_URL,
             ],
             'style-src': ['self', 'unsafe-inline'],
             'img-src': [
                 'self',
                 'data:',
                 'blob:',
-                // process.env.VITE_BUNNYCDN_IMAGE_URL
             ],
             'font-src': ['self'],
             'upgrade-insecure-requests': true,
